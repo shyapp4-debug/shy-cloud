175bd6316a124dba91ecac9394b906ac1b8a36b5
shyapp4-debug <shyapp4@gmail.com>
2026-07-07 11:25:22 -0500

    Update market.py

--- market.py
+++ market.py
@@ -1,28 +1,28 @@
 def get_market_bias(prices):
-bias_score = 0
+  bias_score = 0
 
-if prices.get("QQQ", 0) > prices.get("SPY", 0) * 0.96:
-bias_score += 1
+  if prices.get("QQQ", 0) > prices.get("SPY", 0) * 0.96:
+    bias_score += 1
 else:
-bias_score -= 1
+    bias_score -= 1
 
 if prices.get("QQQ", 0) > 700:
-bias_score += 1
+    bias_score += 1
 else:
-bias_score -= 1
+    bias_score -= 1
 
 if prices.get("SPY", 0) > 725:
-bias_score += 1
+    bias_score += 1
 else:
-bias_score -= 1
+    bias_score -= 1
 
 if bias_score == 3:
-market_bias = "STRONG BULLISH"
+    market_bias = "STRONG BULLISH"
 elif bias_score == 2:
-market_bias = "BULLISH"
+    market_bias = "BULLISH"
 elif bias_score == -3:
-market_bias = "STRONG BEARISH"
+    market_bias = "STRONG BEARISH"
 elif bias_score == -2:
-market_bias = "BEARISH"
+    market_bias = "BEARISH"
 else:
-market_bias = "NEUTRAL / WAIT"
+    market_bias = "NEUTRAL / WAIT"
