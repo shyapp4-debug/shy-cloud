93bfe6a8bc4eac091931d40b89f935b5c0490746
shyapp4-debug <shyapp4@gmail.com>
2026-07-07 19:43:55 -0500

    Update signals.py

--- signals.py
+++ signals.py
@@ -5,20 +5,20 @@ def get_signal(market_bias, prices):
     trade_grade = "NO TRADE"
 
     if market_bias == "STRONG BULLISH":
-      signal = "QQQ BREAKOUT"
-      trade_direction = "CALL"
-      setup_score = 5
-      trade_grade = "A+"
+        signal = "QQQ BREAKOUT"
+        trade_direction = "CALL"
+        setup_score = 5
+        trade_grade = "A+"
 
-  elif market_bias == "STRONG BEARISH":
-      signal = "QQQ BREAKDOWN"
-      trade_direction = "PUT"
-      setup_score = 5
-      trade_grade = "A+"
+    elif market_bias == "STRONG BEARISH":
+        signal = "QQQ BREAKDOWN"
+        trade_direction = "PUT"
+        setup_score = 5
+        trade_grade = "A+"
 
-  return {
-      "signal": signal,
-      "direction": trade_direction,
-      "setup_score": setup_score,
-      "trade_grade": trade_grade
-  }
+    return {
+        "signal": signal,
+        "direction": trade_direction,
+        "setup_score": setup_score,
+        "trade_grade": trade_grade
+    }
