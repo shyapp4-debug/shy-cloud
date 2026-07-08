ecbd48426720564996ce74ac0445f8546372d589
shyapp4-debug <shyapp4@gmail.com>
2026-07-07 22:38:32 -0500

    Fix emailer module

--- emailer.py
+++ emailer.py
@@ -6,13 +6,13 @@ EMAIL_PASSWORD = "vtbv mnpj jzgg ctqy"
 SEND_TO = "cecilshy8@yahoo.com"
 
 def send_email(subject, body):
-msg = MIMEText(body)
-msg["Subject"] = subject
-msg["From"] = EMAIL_ADDRESS
-msg["To"] = SEND_TO
+    msg = MIMEText(body)
+    msg["Subject"] = subject
+    msg["From"] = EMAIL_ADDRESS
+    msg["To"] = SEND_TO
 
-server = smtplib.SMTP("smtp.gmail.com", 587)
-server.starttls()
-server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-server.sendmail(EMAIL_ADDRESS, SEND_TO, msg.as_string())
-server.quit()
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    server.sendmail(EMAIL_ADDRESS, SEND_TO, msg.as_string())
+    server.quit()
