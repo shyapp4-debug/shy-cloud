36db50e5070f626a97c77da8e5a5e02ebae2212b
shyapp4-debug <shyapp4@gmail.com>
2026-07-07 23:55:08 -0500

    Update emailer.py

--- emailer.py
+++ emailer.py
@@ -1,18 +1,24 @@
 import smtplib
 from email.mime.text import MIMEText
 
-EMAIL_ADDRESS = "shyapp4@gmail.com"
-EMAIL_PASSWORD = "vtbv mnpj jzgg ctqy"
-SEND_TO = "cecilshy8@yahoo.com"
-
 def send_email(subject, body):
-    msg = MIMEText(body)
-    msg["Subject"] = subject
-    msg["From"] = EMAIL_ADDRESS
-    msg["To"] = SEND_TO
+    try:
+        sender = "YOUR_EMAIL@gmail.com"
+        password = "YOUR_APP_PASSWORD"
+        receiver = "YOUR_EMAIL@gmail.com"
+        
+        msg = MIMEText(body)
+        msg["Subject"] = subject
+        msg["From"] = sender
+        msg["To"] = receiver
+        
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(sender, password)
+        server.sendmail(sender, receiver, msg.as_string())
+        server.quit()
 
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-    server.sendmail(EMAIL_ADDRESS, SEND_TO, msg.as_string())
-    server.quit()
+        print("✅ Email Sent!")
+
+    except Exception as e:
+        print("Email Error:", e)
